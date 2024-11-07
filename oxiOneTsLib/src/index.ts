2d75a152463a927d831b407818e6f6e70a6e65e5
Wylie Fowler <wylie@myartcache.com>
2024-11-07 12:34:41 -0500

    add lib

---
+++ oxiOneTsLib/src/index.ts
@@ -0,0 +1,12 @@
+import { WebMidi } from "webmidi";
+import { midiWorker } from "./worker";
+
+
+export * from "./worker";
+
+
+// const worker = new midiWorker()
+// worker.init().then(()=>{
+//   console.log(worker.fwVersion);
+//   worker.getProject(0)
+// })
---
+++ oxiOneTsLib/src/worker.ts
@@ -0,0 +1,136 @@
+import { Input, MessageEvent, Output, WebMidi } from "webmidi";
+import { OXI_SYSEX_CAT, OXI_SYSEX_PROJECT, OXI_SYSEX_SYSTEM } from "./OXI_SYSEX_MSG";
+import { deNibblize } from "./nibble";
+import _ from "lodash";
+
+export class midiWorker {
+    private state_: WorkerState_e = WorkerState_e.WORKER_IDLE
+    deviceName = "OXI ONE Port 1"
+    private SYSEX_HEADER = [0xf0,0x00,0x21,0x5b,0x00,0x01]
+    private midiOutput: Output
+    private midiInput: Input
+    private webMidi: any
+    public fwVersion: string
+    private midiPromiseResolve: (value: unknown) => void
+    private midiPromise: Promise<unknown>
+    constructor() {}
+
+    public async init() {
+        try {
+            this.webMidi = await WebMidi.enable({sysex: true})
+            this.midiOutput = WebMidi.getOutputByName(this.deviceName);
+            this.midiInput = WebMidi.getInputByName(this.deviceName);
+            this.midiInput.addListener("midimessage",this.onMidiReceive.bind(this))
+            console.log("midi worker started");
+            this.GetFwVersion()
+            await this.midiPromise
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    GetFwVersion() {
+        this.sendCmd(OXI_SYSEX_CAT.MSG_CAT_SYSTEM,OXI_SYSEX_SYSTEM.MSG_SYSTEM_SW_VERSION)
+    }
+
+    public getState(){
+        return this.state_
+    }
+
+    public async getProject(index:number) {
+        let data: number[] = []
+        data = data.concat(this.SYSEX_HEADER)
+        data.push(OXI_SYSEX_CAT.MSG_CAT_PROJECT)
+        data.push(OXI_SYSEX_PROJECT.MSG_PROJECT_GET_PROJ_HEADER)
+        data.push(index)
+        data.push(0)
+        data.push(0xF7)
+
+        this.midiPromise = new Promise((resolve, reject) => { 
+            this.midiPromiseResolve = resolve
+        })
+        this.midiOutput.send(data)        
+        await this.midiPromise
+    }
+
+     
+
+    public setState(state:WorkerState_e){
+        this.state_ = state
+    }
+
+    private sendCmd(cat:OXI_SYSEX_CAT,id:number){      
+        let data: number[] = []
+        data = data.concat(this.SYSEX_HEADER)
+        data.push(cat)
+        data.push(id)
+        data.push(0xF7)
+
+        this.midiPromise = new Promise((resolve, reject) => { 
+            this.midiPromiseResolve = resolve
+        })
+        this.midiOutput.send(data)
+    }
+
+    private onMidiReceive(event: MessageEvent){        
+        console.log(event.rawData);
+        if (event.message.type === 'sysex') {                        
+            if (_.isEqual(event.rawData.slice(0,6),this.SYSEX_HEADER)) {                
+                switch (event.rawData[6]) {
+                    case OXI_SYSEX_CAT.MSG_CAT_SYSTEM:
+                        switch (event.rawData[7]) {
+                            case OXI_SYSEX_SYSTEM.MSG_SYSTEM_SW_VERSION:
+                                let version = '';
+                                let buffer = []
+                                deNibblize(buffer,event.rawData as unknown as number[],8)
+                                for (let varIndex = 0; varIndex < buffer.length; ++varIndex) {
+                                    const c = String.fromCharCode(buffer[varIndex]);
+                                    if (c === ' ' || c === '\0') {
+                                        version += '\0'; 
+                                        break;
+                                    } else {
+                                        version += c; 
+                                    }
+                                }                                
+                                this.fwVersion = version
+                                break;
+                        
+                            default:
+                                break;
+                        }
+                        break;
+                    case OXI_SYSEX_CAT.MSG_CAT_PROJECT:
+                        switch (event.rawData[7]) {
+                            case OXI_SYSEX_PROJECT.MSG_PROJECT_SEND_PROJ_HEADER:
+                                console.log(event.message); 
+                                break;
+                            default:
+                                console.log(event.message);
+                                break;
+                        }
+                        break
+                    default:
+                        console.log(event.message);
+                        break;
+                }
+            }
+        } 
+        this.midiPromiseResolve(true)
+    }
+}
+
+
+
+
+enum WorkerState_e {
+    WORKER_IDLE,
+    WORKER_CANCELLING,
+    // launch thread
+    WORKER_FW_UPDATE_OXI_ONE,
+    WORKER_FW_UPDATE_BLE,
+    WORKER_FW_UPDATE_SPLIT,
+    WORKER_GET_PROJECT,
+    WORKER_SEND_PROJECT,
+    WORKER_GET_ALL_PROJECTS,
+};
